7b6c4033b4133e119855db488afd2e996bd3b2d6
luangnp98 <31967501+luangnp98@users.noreply.github.com>
2018-12-06 15:54:49 -0200

    Delete Usuario.cpp

--- Usuario.cpp
+++
@@ -1,8 +0,0 @@
-#include "Biblioteca.h"
-
-bool Usuario::operator== (const Usuario& UserVerif) const { // sobrecarga do operador
-	if ((UserVerif.cpf == cpf) && (UserVerif.nome == nome) && (UserVerif.telefone == telefone) && (UserVerif.endereco == endereco))
-		return true;
-	else
-		return false;
-}
